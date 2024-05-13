0e466052b178edba8fbc54b9fb7c13ad35438b2b
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-13 12:00:25 +0900

    2章練習問題を追加

---
+++ 2章練習問題2-2/src/Start.java
@@ -0,0 +1,13 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		MyFrame frame1=new RectFrame();
+		frame1.setLocation(50,300);
+		MyFrame frame2=new RectFrame();
+		frame2.setLocation(100,300);
+
+	}
+
+}
