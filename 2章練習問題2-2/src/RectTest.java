9b776df2181bf42562f56d8b6d32444f29b3031d
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-14 12:11:05 +0900

    五色

--- 2章練習問題2-2/src/RectTest.java
+++ 2章練習問題2-2/src/RectTest.java
@@ -1,9 +1,15 @@
 
 public class RectTest extends MyFrame {
 	public void run() {
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		setColor(170,0,255);
 		fillRect(130,40,50,100);
+		setColor(0,255,255);
 		fillRect(30,200,150,10);
+		setColor(0,0,0);
 		fillRect(90,160,30,30);
+		setColor(127,127,127);
+		fillRect(90,200,30,50);
 	}
 }
