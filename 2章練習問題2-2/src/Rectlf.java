780252f04bd6ae84048b20b2511958daeb285c92
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-20 12:34:12 +0900

    練習問題4-3

--- 2章練習問題2-2/src/Rectlf.java
+++ 2章練習問題2-2/src/Rectlf.java
@@ -1,13 +1,15 @@
 
 public class Rectlf extends MyFrame {
 	public void run() {
-		int x=30,y=10;
-		setColor(0,128,0);
+		int x=30,r=0,g=0,b=0;
 		int i;
 		for(i=0;i<10;i++) {
-			fillRect(x,80,10,y);
+			setColor(r,g,b);
+			fillRect(x,80,10,100);
 			x+=20;
-			y+=20;
+			r+=20;
+			g+=20;
+			b+=20;
 		}
 	}
 
