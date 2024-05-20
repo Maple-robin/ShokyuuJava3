94fcb2083e9298aab1fb1c820f97390b451177f9
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-20 10:19:49 +0900

    練習問題3-1

---
+++ 2章練習問題2-2/src/RectAndOval.java
@@ -0,0 +1,9 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		fillRect(50,100,30,20);
+		fillRect(30,120,70,20);
+		fillOval(40,140,15,15);
+		fillOval(75,140,15,15);
+	}
+}
--- 2章練習問題2-2/src/Start.java
+++ 2章練習問題2-2/src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("プロジェクトを実行します");
-		MyFrame frame1=new TextTest();
+		MyFrame frame1=new RectAndOval();
 	}
 
 }
