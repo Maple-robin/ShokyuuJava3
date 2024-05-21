3b037bb19440bea3e12412f21b8b384c83b3c209
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-21 11:54:37 +0900

    1秒遅い

--- 2章練習問題2-2/src/Start.java
+++ 2章練習問題2-2/src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("プロジェクトを実行します");
-		MyFrame frame1=new 先生課題();
+		MyFrame frame1=new DisplayRectSlow();
 	}
 
 }
