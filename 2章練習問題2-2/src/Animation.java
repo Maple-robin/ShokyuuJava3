6fcd668e54b70b8e079c30259011a4a588175693
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-27 11:41:23 +0900

    先生課題1

--- 2章練習問題2-2/src/Animation.java
+++ 2章練習問題2-2/src/Animation.java
@@ -1,36 +1,38 @@
 public class Animation extends MyFrame {
 	public void run() {
-        int y = 100, x = 30;
-        while (y <= 170) {
-            clear();
-            setColor(0, 128, 0);
-            fillRect(x, y, 10, 100);
-            x+=5;
-            y += 5;
-            sleep(0.1);
-        }
-        while (x >= 30) {
-            clear();
-            setColor(0, 128, 0);
-            fillRect(x, y, 10, 100);
-            x -= 5;
-            sleep(0.1);
-        }
-        while (y >= 100) {
-            clear();
-            setColor(0, 128, 0);
-            fillRect(x, y, 10, 100);
-            x+=5;
-            y -= 5;
-            sleep(0.1);
-        }
-        while (x >= 30) {
-            clear();
-            setColor(0, 128, 0);
-            fillRect(x, y, 10, 100);
-            x -= 5;
-            sleep(0.1);
-        }
-        
+		int y = 100, x = 30,z=130;
+		while (true) {
+
+			while (x <= 170) {
+				clear();
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				x += 5;
+				sleep(0.1);
+			}
+			while (y <= z) {
+				clear();
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				y += 5;
+				sleep(0.1);
+			}
+			z+=30;
+			while (x >= 30) {
+				clear();
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				x -= 5;
+				sleep(0.1);
+			}
+			while (y <= z) {
+				clear();
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				y += 5;
+				sleep(0.1);
+			}
+			z+=30;
+		}
 	}
 }
