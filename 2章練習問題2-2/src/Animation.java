542626fbe1bb6b64408dcd9493bcf06b24018b51
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-27 10:49:52 +0900

    5-1

--- 2章練習問題2-2/src/Animation.java
+++ 2章練習問題2-2/src/Animation.java
@@ -1,24 +1,20 @@
-public class Animation extends MyFrame{
+public class Animation extends MyFrame {
 	public void run() {
-		int y=100,x=30;
-		while(true)
-		{
-		while(x<=170) 
-		{
+		int y = 170, x = 30;
+		while (y >= 100) {
 			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x+=5;
+			setColor(0, 128, 0);
+			fillRect(x, y, 10, 100);
+			y -= 5;
 			sleep(0.1);
 		}
-		while(x>=30) {
+		while (x <= 170) {
 			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x-=5;
+			setColor(0, 128, 0);
+			fillRect(x, y, 10, 100);
+			x += 5;
 			sleep(0.1);
 		}
-		}
-	
+
 	}
 }
