f298fcd763fb5c448ba0466c3c886255c1b7217b
akimarufanboy <zeal23418140@fsgcl.jp>
2024-05-28 11:08:23 +0900

    6-2

--- 2章練習問題2-2/src/DisplayCar.java
+++ 2章練習問題2-2/src/DisplayCar.java
@@ -1,10 +1,14 @@
 
 public class DisplayCar extends MyFrame{
 	public void run() {
-		  Car car1 = new Car(50, 50, 10, 5);
+		  Car car1 = new Car(50, 50, 10, 0);
+		  Car car2= new Car(250, 150, -10,0);
 		for (int i = 0; i <= 30; i++) {
 			clear();
 			car1.draw(this);
+			car2.draw(this);
+			car1.move();
+			car2.move();
 			sleep(0.1);
 		}
 	}
