b37ea5d4ca0494a1335a39f23f17371a839d4a2e
akimarufanboy <zeal23418140@fsgcl.jp>
2024-06-03 10:28:34 +0900

    もう一つ

--- 2章練習問題2-2/src/PracticeCar.java
+++ 2章練習問題2-2/src/PracticeCar.java
@@ -1,23 +1,18 @@
+import java.util.Vector;
 public class PracticeCar extends MyFrame {
 	public void run() {
-		Practice[] car = new Practice[5];
-		car[0] = new Practice(50, 50, 1, 0);
-		car[1] = new Practice(50, 120, 2, 0);
-		car[2] = new Practice(50, 190, 3, 0);
-		car[3] = new Practice(50, 260, 4, 0);
-		car[4] = new Practice(50, 330, 5, 0);
-		for (int i = 0; i <= 30; i++) {
+		Vector<Practice> car=new Vector<Practice>();
+		car.add(new Practice(50,50,1,0));
+		car.add(new Practice(50,120,2,0));
+		car.add(new Practice(50,190,3,0));
+		car.add(new Practice(50,260,4,0));
+		car.add(new Practice(50,330,5,0));
+		for(int i=0;i<30;i++) {
 			clear();
-			car[0].draw(this);
-			car[0].move();
-			car[1].draw(this);
-			car[1].move();
-			car[2].draw(this);
-			car[2].move();
-			car[3].draw(this);
-			car[3].move();
-			car[4].draw(this);
-			car[4].move();
+			for(int j=0;j<car.size();j++) {
+				car.get(j).draw(this);
+				car.get(j).move();
+			}
 			sleep(0.1);
 		}
 	}
