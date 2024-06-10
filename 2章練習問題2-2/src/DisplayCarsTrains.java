5a4d091badc6cdbb15ffea9af9808a196e29c0f4
akimarufanboy <zeal23418140@fsgcl.jp>
2024-06-10 11:37:30 +0900

    練習8-1

--- 2章練習問題2-2/src/DisplayCarsTrains.java
+++ 2章練習問題2-2/src/DisplayCarsTrains.java
@@ -1,10 +1,13 @@
 
 public class DisplayCarsTrains extends MyFrame{
 	public void run() {
-		Vehicle[] Vehicles = new Vehicle[3];
-		Vehicles[0]=new Car(10,30,3,0);
-		Vehicles[1]=new Car(10,100,5,0);
-		Vehicles[2]=new Train(10,150,7,0);
+		Vehicle[] Vehicles = new Vehicle[6];
+		Vehicles[0]=new Car(50,30,3,0);
+		Vehicles[1]=new Car(70,90,-5,0);
+		Vehicles[2]=new Train(100,150,7,0);
+		Vehicles[3]=new Train(50,210,-4,0);
+		Vehicles[4]=new Truck(40,270,5,0);
+		Vehicles[5]=new Truck(100,330,6,0);
 		for(int i=0;i<30;i++) {
 			clear();
 			for(int j=0;j<Vehicles.length;j++) {
