93aead6cb5db3418783a5018958b184cba2f3c3f
akimarufanboy <zeal23418140@fsgcl.jp>
2024-06-10 11:43:23 +0900

    練習8-1直し

--- 2章練習問題2-2/src/DisplayVehicleVector.java
+++ 2章練習問題2-2/src/DisplayVehicleVector.java
@@ -2,9 +2,12 @@ import java.util.Vector;
 public class DisplayVehicleVector extends MyFrame{
 	public void run() {
 		Vector<Vehicle> vehicle=new Vector<Vehicle>();
-		vehicle.add(new Car(10,30,3,0));
-		vehicle.add(new Car(10,100,5,0));
-		vehicle.add(new Car(10,150,7,0));
+		vehicle.add(new Car(50,30,3,0));
+		vehicle.add(new Car(70,90,-5,0));
+		vehicle.add(new Train(100,150,7,0));
+		vehicle.add(new Train(50,210,-4,0));
+		vehicle.add(new Truck(40,270,5,0));
+		vehicle.add(new Truck(100,330,6,0));
 		
 		for(int i=0;i<30;i++) {
 			clear();
--- 2章練習問題2-2/src/Start.java
+++ 2章練習問題2-2/src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("プロジェクトを実行します");
-		MyFrame fame1=new DisplayCarsTrains();
+		MyFrame fame1=new DisplayVehicleVector();
 	}
 
 }
